8e276ae17bdbfbfb733ef3c56309eb6f0afbdb23
amanshahi <amanshahi2016@gmail.com>
2018-04-28 22:08:28 +0530

    changed file

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from init_data import *
 from multiprocessing import Pool
-obj = model('epinion_trust_with_timestamp.mat', 'trust', 2, 10, 1, 1, 1, 2)
+obj = model('epinion_trust_with_timestamp.mat', 'trust', 2, 10, 1, 1, 1, 2, 10**6)
 obj.find_trust_at_times()
 obj.model_train(3)
 # pool = Pool()
